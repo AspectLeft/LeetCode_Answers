77e1a2f1e0bb74a01afbaab478f94e2b691b5e4d
AspectLeft <aspectleft@gmail.com>
2017-08-14 13:48:07 +0800

    Create Valid_Parentheses.java

---
+++ Valid_Parentheses.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case '(':
+                case '[':
+                case '{':
+                    stack.push(c);
+                    break;
+                case ')':
+                    if (stack.empty() || stack.pop() != '(') {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (stack.empty() || stack.pop() != '[') {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (stack.empty() || stack.pop() != '{') {
+                        return false;
+                    }
+                    break;
+            }
+        }
+
+        return stack.empty();
+    }
+}
