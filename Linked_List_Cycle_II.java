2f046472004ac9c5faaf75cdb2b34bb690d22524
AspectLeft <aspectleft@gmail.com>
2018-10-16 17:48:19 -0700

    Create Linked_List_Cycle_II.java

---
+++ Linked_List_Cycle_II.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) return null;
+        ListNode p = head, q = head;
+        while (q != null && q.next != null) {
+            p = p.next;
+            q = q.next.next;
+            if (p == q) {
+                ListNode r = head;
+                while (r != q) {
+                    r = r.next;
+                    q = q.next;
+                }
+                return r;
+            }
+        }
+        return null;
+    }
+}
